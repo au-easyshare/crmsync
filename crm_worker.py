f5ae004d68c35e62420381e9dad63699c669dd90
Rob Fowler <legal@mianos.com>
2015-05-22 21:40:59 +1000

    working

--- crm_worker.py
+++ crm_worker.py
@@ -3,16 +3,12 @@ import json
 
 # gm_worker = gearman.GearmanWorker(['prod1.internal.easy-share.com.au'])
 
-otable = {'user': puser}
+#otable = {'user': puser}
 
 class CRMWorker:
-    def __init__(self, options, gm_worker):
-        app_engine = create_engine(options.uri)
-        adjust_schemas(staging=options.staging_schema, model=options.model_schema)
-        make_session = sessionmaker(bind=app_engine)  # autocommit=True)
-        self.session = make_session()
-
+    def __init__(self, options, gm_worker, session):
         gm_worker.register_task('gear_beta', functools.partial(CRMWorker.gm_task, self))
+        self.session = session
 
     def gm_task(self, gearman_worker, gearman_job):
         print 'job', gearman_job
@@ -26,6 +22,3 @@ class CRMWorker:
         else:
             return json.dumps(dict(status='OK'))
         return ret
-    # gm_worker.set_client_id('your_worker_client_id_name')
-    # gm_worker.register_task('gear_beta', task_crm)
-    # gm_worker.work()
--- gsync.py
+++ gsync.py
@@ -4,8 +4,12 @@ import ConfigParser
 import optparse
 import gearman
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from magic_options import MagicOptions
 
+from es_sqla.dbase import adjust_schemas
 from crm_worker import CRMWorker
 
 config_defaults = {}
@@ -18,8 +22,8 @@ if __name__ == '__main__':
     parser.add_option("-f", "--config", dest="config", default='gsync.ini', help="config file")
     parser.add_option("-s", "--section", dest="section", default='beta', help="config file section")
     parser.add_option("-t", "--test", dest="test", action="store_true", default=None, help="Send test")
-    options, args = parser.parse_args()
-    config.readfp(open(options.config))
+    cmd_line_opts, args = parser.parse_args()
+    config.readfp(open(cmd_line_opts.config))
 
     config_defaults = {'odoo_host': 'localhost',
                        'odoo_protocol': 'xmlrpc',
@@ -27,7 +31,7 @@ if __name__ == '__main__':
                        'odoo_user': 'rob@easy-share.com.au',
                        'odoo_db': 'easy1'}
 
-    mopts = MagicOptions(config_defaults, dict(config.items(options.section)), vars(options))
+    mopts = MagicOptions(config_defaults, dict(config.items(cmd_line_opts.section)), vars(cmd_line_opts))
 
     if mopts.test:
         client = gearman.GearmanClient(mopts.gearman.split(','))
@@ -35,6 +39,10 @@ if __name__ == '__main__':
         print "submitted some test data to ", mopts.service, ".. Exiting"
         sys.exit(1)
 
+    app_engine = create_engine(mopts.uri)
+    adjust_schemas(staging=mopts.staging_schema, model=mopts.model_schema)
+    make_session = sessionmaker(bind=app_engine)  # autocommit=True)
+
     gm_worker = gearman.GearmanWorker(mopts.gearman.split(','))
-    crm_task = CRMWorker(options, gm_worker)
+    crm_task = CRMWorker(mopts, gm_worker, make_session())
     gm_worker.work()
