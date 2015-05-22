f46663c12d9567f3bb7cfb94fa0fec2f6426f71e
Rob Fowler <legal@mianos.com>
2015-05-22 21:19:27 +1000

    int

--- crm_worker.py
+++ crm_worker.py
@@ -3,16 +3,23 @@ import json
 
 # gm_worker = gearman.GearmanWorker(['prod1.internal.easy-share.com.au'])
 
+otable = {'user': puser}
 
 class CRMWorker:
     def __init__(self, options, gm_worker):
+        app_engine = create_engine(options.uri)
+        adjust_schemas(staging=options.staging_schema, model=options.model_schema)
+        make_session = sessionmaker(bind=app_engine)  # autocommit=True)
+        self.session = make_session()
+
         gm_worker.register_task('gear_beta', functools.partial(CRMWorker.gm_task, self))
 
     def gm_task(self, gearman_worker, gearman_job):
         print 'job', gearman_job
         try:
             jsd = json.loads(gearman_job.data)
-            print "incoming", str(jsd)
+            if 'object' not in jsd:
+                raise Exception("no 'object'")
         except Exception as ee:
             print "bad", str(ee)
             ret = json.dumps(dict(status='error', message=str(ee)))
