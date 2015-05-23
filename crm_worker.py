ec6709d22221baf5af48262330d75d074a050c18
Rob Fowler <legal@mianos.com>
2015-05-23 21:45:03 +1000

    made json type dispatcher

--- crm_worker.py
+++ crm_worker.py
@@ -6,6 +6,17 @@ import oerplib
 
 #otable = {'user': puser}
 
+def handle_lead(jsd, erp_con):
+    pass
+
+def handle_user(jsd, erp_con):
+    pass
+
+objects = {
+        'lead': handle_lead,
+        'user': handle_user
+        }
+
 class CRMWorker:
     def __init__(self, options, gm_worker, session):
         gm_worker.register_task('gear_beta', functools.partial(CRMWorker.gm_task, self))
@@ -19,6 +30,9 @@ class CRMWorker:
             jsd = json.loads(gearman_job.data)
             if 'object' not in jsd:
                 raise Exception("no 'object'")
+            if jsd['object'] not in objects:
+                raise Exception("unsupported object '%s'" % jsd['object'])
+            objects[jsd['object']](jsd, self.erp_con)
         except Exception as ee:
             print "bad", str(ee)
             ret = json.dumps(dict(status='error', message=str(ee)))
