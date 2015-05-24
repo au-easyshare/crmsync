391f9bec1e21f0ffd5513720c568fd19bd6d7c65
Rob Fowler <legal@mianos.com>
2015-05-24 22:01:35 +1000

    Added lead for new email

--- crm_worker.py
+++ crm_worker.py
@@ -4,18 +4,23 @@ import oerplib
 
 # gm_worker = gearman.GearmanWorker(['prod1.internal.easy-share.com.au'])
 
-#otable = {'user': puser}
+# otable = {'user': puser}
 
-def handle_lead(jsd, erp_con):
-    pass
+def handle_lead(jsd, crm):
+    p_obj = crm.oerp.get('res.partner')
+    email = jsd['email']
+    es_users = p_obj.search([('email', '=', email)])
+    if not es_users:
+        # user_id for flase to make it unassigned
+        lead_id = crm.oerp.create('crm.lead', {'user_id': False, 'name': email, 'email': email, 'type': 'lead'})
 
-def handle_user(jsd, erp_con):
+def handle_user(jsd, oerp):
     pass
 
 objects = {
-        'lead': handle_lead,
-        'user': handle_user
-        }
+    'lead': handle_lead,
+    'user': handle_user
+}
 
 class CRMWorker:
     def __init__(self, options, gm_worker, session):
@@ -23,6 +28,8 @@ class CRMWorker:
         self.session = session
         self.oerp = oerplib.OERP(options.odoo_host, protocol=options.odoo_protocol, port=options.odoo_port)
         self.erp_con = self.oerp.login(options.odoo_user, options.odoo_password, options.odoo_db)
+        self.template_user_id = self.oerp.search('res.partner', [('name', '=', 'Template User')])[0]
+        self.template_user = self.oerp.read('res.partner', [self.template_user_id])
 
     def gm_task(self, gearman_worker, gearman_job):
         print 'job', gearman_job
@@ -32,7 +39,7 @@ class CRMWorker:
                 raise Exception("no 'object'")
             if jsd['object'] not in objects:
                 raise Exception("unsupported object '%s'" % jsd['object'])
-            objects[jsd['object']](jsd, self.erp_con)
+            objects[jsd['object']](jsd, self)
         except Exception as ee:
             print "bad", str(ee)
             ret = json.dumps(dict(status='error', message=str(ee)))
