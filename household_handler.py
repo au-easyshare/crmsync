cf6edf4e1c7389b87493f7fd716a468fc80d055b
Rob Fowler <legal@mianos.com>
2015-05-25 16:39:43 +1000

    houshold handler

---
+++ household_handler.py
@@ -0,0 +1,15 @@
+from es_sqla.model_schema import app
+
+def handle(jsd, crm):
+    from ipdb import set_trace; set_trace()
+    user_id = jsd['id']['user']
+    household_id = jsd['id']['household']
+
+    # first get the user, so we can change the lead to a user
+    user_details = crm.session.query(app.User, app.UserTerm, app.Household, app.HouseholdRent, app.TrustAccount) \
+                      .outerjoin(app.UserTerm, app.UserTerm.user_id == app.User.id) \
+                      .outerjoin(app.Household, app.Household.id == app.UserTerm.household_id) \
+                      .outerjoin(app.HouseholdRent, app.HouseholdRent.household_id == app.Household.id) \
+                      .outerjoin(app.TrustAccount, app.Household.trust_account_id == app.TrustAccount.id) \
+                      .filter(app.User.id == user_id, app.Household.id == household_id).first()
+
