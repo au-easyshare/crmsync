b370e0131b96cab6976dcc7401940d3d9c77cd86
Rob Fowler <rob@easy-share.com.au>
2015-08-25 13:33:45 +1000

    updates for working code

--- sync.py
+++ sync.py
@@ -1,20 +1,31 @@
 import datetime
 from es_sqla.model_schema import app
+from sqlalchemy import func
 
 from fmapper import field_map
 
 
 def sync(app_session, oerp):
-    qry = app_session.query(app.User, app.UserTerm, app.Household, app.HouseholdRent, app.TrustAccount, app.Address) \
-                     .outerjoin(app.UserTerm, app.UserTerm.user_id == app.User.id) \
-                     .outerjoin(app.Household, app.Household.id == app.UserTerm.household_id) \
+    max_term = app_session.query(app.UserTerm.household_id, app.UserTerm.user_id, app.UserTerm.id.label('term_id'),
+                                 func.max(app.UserTerm.term_start).label('term_start')) \
+                          .group_by(app.UserTerm.user_id).subquery()
+
+    qry = app_session.query(app.User, app.Household, app.HouseholdRent, app.TrustAccount, app.Address,
+                            max_term.c.household_id, max_term.c.term_id, max_term.c.term_start) \
+                     .outerjoin(max_term, max_term.c.user_id == app.User.id) \
+                     .outerjoin(app.Household, app.Household.id == max_term.c.household_id) \
                      .outerjoin(app.HouseholdRent, app.HouseholdRent.household_id == app.Household.id) \
                      .outerjoin(app.TrustAccount, app.Household.trust_account_id == app.TrustAccount.id) \
                      .outerjoin(app.Address, app.Address.id == app.Household.address_id)
 
     product_name_to_id = dict([(ii.code, ii.id) for ii in oerp.get('product.product') if ii.code])
     p_obj = oerp.get('res.partner')
+    limit = -1
     for res in qry:
+        if not limit:
+            print "stopping from limit"
+            break
+        limit -= 1
         es_users = p_obj.search([('es_user_id', '=', res.User.id)])
 
         rec_partner = dict()
@@ -30,14 +41,15 @@ def sync(app_session, oerp):
                 else:
                     rec_partner[field] = mapper(res)
             except ValueError as ee:
+                pass
                 print "nope for ", field, "error '%s" % str(ee)
             except AttributeError as ee:
+                pass
                 print "missing attribute for ", field, "error '%s" % str(ee)
 
         if not es_users:
             oerp.create('res.partner', rec_partner)
         else:
-            print "Update"
             user = oerp.browse('res.partner', es_users[0])
             new = 0
             updated = 0
@@ -50,17 +62,31 @@ def sync(app_session, oerp):
                     continue
 
                 crm_val = getattr(user, field)
-                if isinstance(crm_val, datetime.datetime) or isinstance(crm_val, datetime.date):
-                    crm_val = str(crm_val)
+                if type(crm_val) != type(value):
+                    if isinstance(crm_val, datetime.datetime) or isinstance(crm_val, datetime.date):
+                        crm_val = str(crm_val)
+                    elif isinstance(crm_val, str) and isinstance(value, long):
+                        value = str(value)
 
-                if crm_val == value:
+                # this can be a single if but it's clearer the first is an ORM field
+                if hasattr(crm_val, 'id') and crm_val.id == value:
+                    same += 1
+                    continue
+                elif isinstance(crm_val, bool) and crm_val is False and value is None:
+                    same += 1
+                    continue
+                elif crm_val == value:
                     same += 1
                     continue
 
-                print "Update field '%s' value '%s'" % (field, str(value))
+                print "Update field '%s' value '%s' crm '%s'" % (field, str(value), crm_val)
+                print "es user", res.User.id, "orm user ", user.id
+                #if field is 'es_household_rent_id':
+                #    from IPython import embed; embed()
                 setattr(user, field, value)
                 updated += 1
 
-            print "user ", user.id, "new", new, "updated", updated, "same", same
-            print "user", user
+            print "user", user, "id ", user.id, "new", new, "updated", updated, "same", same
+            if updated:
+                from IPython import embed; embed()
             oerp.write_record(user)
