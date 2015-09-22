a2dcdec21f2c694267c1e10fbbf79dab18a3d78f
Rob Fowler <rob@easy-share.com.au>
2015-09-22 13:55:54 +1000

    Fixes for es_rental period to be the identifier

--- sync.py
+++ sync.py
@@ -25,7 +25,7 @@ def sync(app_session, oerp):
             print "stopping from limit"
             break
         limit -= 1
-        es_users = p_obj.search([('es_user_id', '=', res.User.id)])
+        es_user_term = p_obj.search([('es_user_term_id', '=', res.UserTerm.id)])
 
         rec_partner = dict()
         for field, mapper in field_map.iteritems():
@@ -46,21 +46,21 @@ def sync(app_session, oerp):
                 pass
                 #print "missing attribute for ", field, "error '%s" % str(ee)
 
-        if not es_users:
+        if not es_user_term:
             oerp.create('res.partner', rec_partner)
         else:
-            user = oerp.browse('res.partner', es_users[0])
+            user_term = oerp.browse('res.partner', es_user_term[0])
             new = 0
             updated = 0
             same = 0
             for field, value in rec_partner.iteritems():
-                if not hasattr(user, field):
-                    setattr(user, field, value)
+                if not hasattr(user_term, field):
+                    setattr(user_term, field, value)
                     print "new (%s, %s)" % (field, str(value))
                     new += 1
                     continue
 
-                crm_val = getattr(user, field)
+                crm_val = getattr(user_term, field)
                 if type(crm_val) != type(value):
                     if isinstance(crm_val, datetime.datetime) or isinstance(crm_val, datetime.date):
                         crm_val = str(crm_val)
@@ -82,13 +82,14 @@ def sync(app_session, oerp):
                     continue
 
                 print "Update field '%s' value '%s' crm '%s'" % (field, str(value), crm_val)
-                print "es user", res.User.id, "orm user ", user.id
+                print "es user_term", res.User.id, "orm user_term ", user_term.id
+                from ipdb import set_trace; set_trace()
                 #if field is 'es_household_rent_id':
                 #    from IPython import embed; embed()
-                setattr(user, field, value)
+                setattr(user_term, field, value)
                 updated += 1
 
-            print "user", user, "id ", user.id, "new", new, "updated", updated, "same", same
+            print "user_term", user_term, "id ", user_term.id, "new", new, "updated", updated, "same", same
 #            if updated:
 #                from IPython import embed; embed()
-            oerp.write_record(user)
+            oerp.write_record(user_term)
